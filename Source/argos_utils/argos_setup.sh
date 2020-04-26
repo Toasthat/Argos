740bfa540a4a6898c4986af1d1b1be2557e685f1
Joshua Ferguson <joshua.ferguson.273@gmail.com>
2020-04-26 14:19:25 -0500

    added a few git ignores

--- Source/argos_utils/argos_setup.sh
+++ Source/argos_utils/argos_setup.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/bash
-
+ARGOS_HOME="$(git rev-parse --show-toplevel)"
+#set environment variable for bash and fish if available
 echo "export ARGOS_HOME=$(git rev-parse --show-toplevel)">>~/.profile
 
 if [ -x "$(command -v fish)" ]; then
     fish -c "set -Ux ARGOS_HOME $(git rev-parse --show-toplevel)"
 fi
 
+#the facial recognition task is necessary for the base security routine
+#it makes sense to download these models by default
+wget -P "$ARGOS_HOME/Storage/models/face_detection/" https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_front
+alface_alt.xml
+
+wget -P "$ARGOS_HOME/Storage/models/face_detection/" https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_front
+alface_default.xml
+
+wget -P "$ARGOS_HOME/Storage/models/face_detection/" https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_eye.xml
