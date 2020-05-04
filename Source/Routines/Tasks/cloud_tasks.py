2fcf22dcac1152cafffe09f3406a817a885f34cc
Parker Mathis <parkermathis@comcast.net>
2020-05-03 19:43:52 -0500

    Reconfigured syntax in files mostly for PyLinter
    Did a bunch of method name changes, added some basic docs to class and methods, and removed redundant face_detection.py file from directory

--- Source/Routines/Tasks/cloud_tasks.py
+++ Source/Routines/Tasks/cloud_tasks.py
@@ -1,10 +1,17 @@
-import sys
+"""This file is used to hold a class whose object
+is initialized with the google drive authentication
+credentials and then contains the methods to manipulate
+the drive files and upload images."""
+
+#import sys
 import os
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
 class CTclass:
+    """this class has been explained above inside
+    of the docfile"""
     def __init__(self, nameIn, fileCreds):
         #Everything in the init should be necessary
         #for an object to be properly initialized.
@@ -12,14 +19,14 @@ class CTclass:
         #that you pass in, and fileCreds is the file
         #supposedly named "myCreds.txt" that you pass in
         #What's initialized here is the name and
-        #google authentication, 
-        
+        #google authentication,
+
         #name=sys.argv[1]
         try:
             self.name = nameIn
-            self.objCreds = fileCreds
+            self.obj_creds = fileCreds
             self.gauth = GoogleAuth()
-            self.gauth.LoadCredentialsFile(self.objCreds)
+            self.gauth.LoadCredentialsFile(self.obj_creds)
             if self.gauth.credentials is None:
                 # Authenticate if they're not there
                 self.gauth.LocalWebserverAuth()
@@ -30,16 +37,20 @@ class CTclass:
                 # Initialize the saved creds
                 self.gauth.Authorize()
             # Save the current credentials to a file
-            self.gauth.SaveCredentialsFile(self.objCreds)
-            self.drive=GoogleDrive(self.gauth)
-        except Exception as exc:
-            #This exception will trigger if something 
+            self.gauth.SaveCredentialsFile(self.obj_creds)
+            self.drive = GoogleDrive(self.gauth)
+        except AttributeError as exc:
+            #This exception will trigger if something
             #fails during initialization
             print('ERROR IN CTclassINIT:', str(exc))
-        
-    def imgUpload(self, title, img):
+
+    def img_upload(self, title):
+        """This method essentially uploads
+        and image to google drive using the
+        given credentials. Need to add ability
+        to this method to create more descriptive
+        and flexible file naming"""
         photo = self.drive.CreateFile({'title':title})
         photo.SetContentFile(self.name)
         photo.Upload()
         os.remove(self.name)
-
--- Source/Routines/Tasks/face_tasks.py
+++ Source/Routines/Tasks/face_tasks.py
@@ -1,13 +1,19 @@
-import cv2 
-import numpy as np
+"""Holds the class that makes objects which perform
+the acts of noticing faces"""
+
+import cv2
+#import numpy as np
 
 
-class faceCascadeDetector(object):
+class FaceCascadeDetector(object):
+    """class which is passed an image of a face?"""
     def __init__(self, face_data):
         self.face_cascade = cv2.CascadeClassifier(face_data["faceCascadePath"])
         self.eye_cascade = cv2.CascadeClassifier(face_data["eyeCascadePath"])
 
-    def getFaces(self, gray_frame):
+    def get_faces(self, gray_frame):
+        """is passed a frame that is tested
+        for faces"""
         return self.face_cascade.detectMultiScale(
             gray_frame, 1.3, 5, minSize=(120, 120)
         )
